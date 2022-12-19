1f08e7ee95eaf3f902a92ae933d212c405fe3549
gznunes <gznunes@gmail.com>
2022-12-18 23:13:26 -0300

    commit 7

--- db.js
+++ db.js
@@ -1,11 +1,20 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    host: 'localhost',
+    host: 'dpg-cefs7kta499e21r5i6f0-a.oregon-postgres.render.com',
     port: 5432,
-    user: 'postgres',
-    password: 'admin',
-    database: 'crud_produtos'
+    user: 'livros_k06r_user',
+    password: 'uhhtkljEHkKwityaDJS6Hy51IF6TXCUI',
+    database: 'livros_k06r',
+    ssl: true
 });
+
+// const pool = new Pool({
+//     host: 'localhost',
+//     port: 5432,
+//     user: 'postgres',
+//     password: 'admin',
+//     database: 'crud_produtos'
+// });
 
 module.exports = pool;
