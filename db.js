8b4795eba49015ab524d1b551701f0a0aafdf06d
gznunes <gznunes@gmail.com>
2023-04-01 14:16:48 -0300

    new database

--- db.js
+++ db.js
@@ -1,11 +1,20 @@
 const Pool = require("pg").Pool;
 
+// const pool = new Pool({
+//     host: 'dpg-cefs7kta499e21r5i6f0-a.oregon-postgres.render.com',
+//     port: 5432,
+//     user: 'livros_k06r_user',
+//     password: 'uhhtkljEHkKwityaDJS6Hy51IF6TXCUI',
+//     database: 'livros_k06r',
+//     ssl: true
+// });
+
 const pool = new Pool({
-    host: 'dpg-cefs7kta499e21r5i6f0-a.oregon-postgres.render.com',
+    host: 'dpg-cgk6304eoogkndhanapg-a.oregon-postgres.render.com',
     port: 5432,
-    user: 'livros_k06r_user',
-    password: 'uhhtkljEHkKwityaDJS6Hy51IF6TXCUI',
-    database: 'livros_k06r',
+    user: 'chamada_user',
+    password: 'c4iuA4u6HRFWyNz8GynJtb2dkARfJqhD',
+    database: 'chamada',
     ssl: true
 });
 
