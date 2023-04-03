728a16cc1d0c3e837e2c012488841d914fa5c3ca
gznunes <gznunes@gmail.com>
2023-04-03 00:51:15 -0300

    rollback

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post("/chamada", async(req, res) => {
         const sql = "INSERT INTO CHAMADA(sala,nome) VALUES ($1,$2) RETURNING *"
         const values = [chamada.sala,chamada.nome]
         const novaChamada = await pool.query(sql, values)
-        res.json(novaChamada.rows[0],"sucesso");
+        res.json(novaChamada.rows[0]);
     } catch (err) {
         console.error(err.message);
     }
