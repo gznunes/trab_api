3ec3ac643b5231e3e7c74676a5fb2ede06d048b6
gznunes <gznunes@gmail.com>
2023-07-02 04:55:17 -0300

    format 2

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ app.post("/chamada", async(req, res) => {
             timeZone: "America/New_York"
         });
         timepoa = f.format(chamada.data);
-        console.log(chamada.data)
-        console.log(timepoa)
+        console.log(chamada.data);
+        console.log(timepoa);
         const sql = "INSERT INTO CHAMADA(sala,nome,dia) VALUES ($1,$2,$3) RETURNING *"
         const values = [chamada.sala,chamada.nome,chamada.data]
         const novaChamada = await pool.query(sql, values)
