06ff4efb07357a1de5e8b0cdc51719bee5506b13
gznunes <gznunes@gmail.com>
2023-07-02 04:07:20 -0300

    date

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ app.post("/chamada", async(req, res) => {
         const chamada = req.body;
         // const sql = "INSERT INTO CHAMADA(sala,nome) VALUES ($1,$2) RETURNING *"
         // const values = [chamada.sala,chamada.nome]
-        const sql = "INSERT INTO CHAMADA(sala,nome) VALUES ($1,$2) RETURNING *"
-        const values = [chamada.sala,chamada.nome]
+        const sql = "INSERT INTO CHAMADA(sala,nome,dia) VALUES ($1,$2,$3) RETURNING *"
+        const values = [chamada.sala,chamada.nome,chamada.data]
         const novaChamada = await pool.query(sql, values)
         res.json(novaChamada.rows[0]);
     } catch (err) {
